05179c7accdc25ec8844a234f0f030f2b1983ef1
Forrest Marshall <forrest@mimirblockchain.solutions>
2017-10-16 16:33:11 -0400

    added newtype struct for hash output to allow custom impls

--- src/hash.rs
+++ src/hash.rs
@@ -11,13 +11,19 @@
 pub mod keccak256 {
     use tiny_keccak::Keccak;
 
+    /// Wrapper-type representing the output of a `keccak-256` hash.
+    #[derive(Debug,Clone,PartialEq,Eq)]
+    pub struct Hash([u8;32]);
+
+    impl_byte_array!(Hash,32);
+
     /// generate the `keccak-256` hash of a piece of data.
     /// if you are trying to hash multiple discontiguous
     /// values together, use `hash_many` instead.
-    pub fn hash<T>(data: T) -> [u8;32] where T: AsRef<[u8]> {
+    pub fn hash<T>(data: T) -> Hash where T: AsRef<[u8]> {
         let mut buffer = [0u8;32];
         Keccak::keccak256(data.as_ref(),&mut buffer);
-        buffer
+        buffer.into()
     }
 
     /// generate the `keccak-256` hash of a slice of multiple
@@ -26,14 +32,14 @@ pub mod keccak256 {
     /// between elements must be added prior.
     /// if you are trying to hash a single contiguous value,
     /// use `hash` instead.
-    pub fn hash_many<T>(data: &[T]) -> [u8;32] where T: AsRef<[u8]> {
+    pub fn hash_many<T>(data: &[T]) -> Hash where T: AsRef<[u8]> {
         let mut keccak = Keccak::new_keccak256();
         let mut buffer = [0u8;32];
         for itm in data {
             keccak.update(itm.as_ref());
         }
         keccak.finalize(&mut buffer);
-        buffer
+        buffer.into()
     }
     
 }
